0abb9b7c24c7c3bb287836d18d4cc8b8c4a22f04
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-03-16 11:07:00 +0300

    ViteJS

--- viteJS/src/App.jsx
+++ viteJS/src/App.jsx
@@ -4,11 +4,15 @@ import Button from './components/Button'
 function App() {
   const [count, setCount] = useState(0)
 
+  const addClick = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <div className='w-[400px] h-[200px] bg-slate-400'>
-        <h1>{count}</h1>
-        <Button />
+      <div className='w-[400px] h-[200px] bg-slate-400 text-center rounded-2xl'>
+        <h1 className="text-[24px] text-red-600 mt-9">{count}</h1>
+        <Button click={addClick}/>
       </div>
     </div>
   )
--- viteJS/src/components/Button.jsx
+++ viteJS/src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button({click}) {
   return (
-    <button>Click</button>
+    <button
+    className="w-[120px] h-20[px] 
+    bg-green-400 text-lg
+    hover:bg-green-600 
+    transition ease rounded-md
+    mt-12"
+    onClick={click}
+    >
+    Click
+    </button>
   )
 }
