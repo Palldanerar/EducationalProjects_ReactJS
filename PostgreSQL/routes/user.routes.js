a99525433aa5a297934a6463ee68cd602dfc4be4
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-04-04 21:09:36 +0300

    v1

--- PostgreSQL/routes/user.routes.js
+++ PostgreSQL/routes/user.routes.js
@@ -5,7 +5,7 @@ const router = new Router();
 router.post("/user", userController.createUser);
 router.get("/user", userController.getUsers);
 router.get("/user/:id", userController.getOneUser);
-router.put("/user", userController.getOneUser);
+router.put("/user", userController.updateUser);
 router.delete("/user/:id", userController.deleteUser);
 
 module.exports = router;
