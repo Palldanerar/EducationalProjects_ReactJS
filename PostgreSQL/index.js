c10e1ed4b72300a9e2ebe198a1ad285e1b6ac1a8
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-04-04 17:08:37 +0300

    v1

--- PostgreSQL/index.js
+++ PostgreSQL/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.POST || 8080;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Мой перывй сервер!");
+  res.send("Мой первывй сервер!");
 });
 
 app.listen(PORT, () => {
