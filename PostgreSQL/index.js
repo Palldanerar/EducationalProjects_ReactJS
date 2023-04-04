1d0db512e90d791570ede343f13c9fc928251f26
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-04-04 21:31:50 +0300

    v1

--- PostgreSQL/index.js
+++ PostgreSQL/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = require("./routes/user.routes");
-
+const postRouter = require("./routes/post.router");
 const PORT = process.env.POST || 8080;
 
 const app = express();
@@ -10,7 +10,9 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
+
 app.use("/api", userRouter);
+app.use("/api", postRouter);
 
 app.listen(PORT, () => {
   console.log(`Сервер работает на ${PORT} порту`);
