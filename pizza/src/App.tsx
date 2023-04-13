f61a9d41cb1e9c108db455d604a5e42137099f2c
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-04-13 11:40:28 +0300

    pizza

--- pizza/src/App.tsx
+++ pizza/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">New App!</div>;
+  return <div className="App">New App</div>;
 }
 
 export default App;
