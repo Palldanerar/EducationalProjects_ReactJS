57e3e4bdce2d4c267e477f2ce673c8e5d2cfe2b8
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-06-09 22:35:05 +0300

    kanban

--- kanban/src/App.tsx
+++ kanban/src/App.tsx
@@ -1,5 +1,25 @@
+import { Heading, Container } from "@chakra-ui/react";
+
 function App() {
-  return <h1>Hi world</h1>;
+  return (
+    <>
+      <Heading
+        fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }}
+        fontWeight="bold"
+        textAlign="center"
+        bgGradient="linear(to-l, rgb(28, 215, 221), rgb(168, 93, 204))"
+        bgClip="text"
+        mt={4}
+      >
+        Kanban on React.JS
+      </Heading>
+      <Container
+        maxWidth="container.lg"
+        px={4}
+        py={10}
+      ></Container>
+    </>
+  );
 }
 
 export default App;
---
+++ kanban/src/utils/enums.ts
@@ -0,0 +1,6 @@
+export enum ColumnType {
+  TO_DO = "todo",
+  IN_PROGRESS = "In Progress",
+  BLOCKED = "Blocked",
+  COMPLETED = "Completed",
+}
